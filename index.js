64ac49400031b491d60b2e88a1efe2c45b20e3c9
zhangding <ding2zhang@163.com>
2017-02-16 11:04:04 +0800

    the origin version

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-/**
- * Created by SWSD on 2017-02-08.
- */
-import React from 'react';
-import ReactDOM from 'react-dom';
-function FancyBorder(props) {
-    return (
-        <div className={'FancyBorder FancyBorder-' + props.color}>
-        {props.children}
-</div>
-);
-}
-
-
+zhangding is handsome
 
