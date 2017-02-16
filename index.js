cba9864e4fbd4a2a47586c7444568b8251b32582
zhangding <ding2zhang@163.com>
2017-02-16 09:34:15 +0800

    add a js filr

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by SWSD on 2017-02-08.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+</div>
+);
+}
+
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+        Welcome
+        </h1>
+        <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+    </p>
+    </FancyBorder>
+);
+}
+
+ReactDOM.render(
+<WelcomeDialog />,
+    document.getElementById('root')
+);
+
+
