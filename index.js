6b4b02da131334063dc8a1db4088a04065ee83ae
zhangding <ding2zhang@163.com>
2017-02-16 09:44:12 +0800

    modification of index.js

--- index.js
+++ index.js
@@ -11,22 +11,5 @@ function FancyBorder(props) {
 );
 }
 
-function WelcomeDialog() {
-    return (
-        <FancyBorder color="blue">
-        <h1 className="Dialog-title">
-        Welcome
-        </h1>
-        <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
-    </p>
-    </FancyBorder>
-);
-}
-
-ReactDOM.render(
-<WelcomeDialog />,
-    document.getElementById('root')
-);
 
 
